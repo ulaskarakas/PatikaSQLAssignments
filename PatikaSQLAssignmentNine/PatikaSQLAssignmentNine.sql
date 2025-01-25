dcc3cb6b1176004176af13ea3924569d82bd8777
ulaskarakas <ulaskarakas95@gmail.com>
2025-01-25 11:16:05 +0300

    Add new files for assignment 9,10 & 11

---
+++ PatikaSQLAssignmentNine/PatikaSQLAssignmentNine.sql
@@ -0,0 +1,12 @@
+-- 1. city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city.city, country.country
+FROM city 
+JOIN country ON country.country_id = city.country_id;
+-- 2. customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name
+FROM payment
+JOIN customer ON customer.customer_id = payment.customer_id;
+-- 3. customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT rental.rental_id, customer.first_name, customer.last_name
+FROM rental
+JOIN customer ON customer.customer_id = rental.rental_id;
